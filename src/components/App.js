5ed8b1d96d691fa4770b50ed9b460f803f83271f
sinbox0701 <psh090953@gmail.com>
2020-11-12 16:26:40 +0900

    delete & update

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,12 @@ import {authService} from "fbase";
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userObj, setUserObj] = useState(null);
 
   useEffect(() => {
     authService.onAuthStateChanged((user) => { // user가 있는지 확인하는 함수
       if(user){
-        setIsLoggedIn(true);
         setUserObj(user);
-      }
-      else {
-        setIsLoggedIn(false);
       }
       setInit(true);
     });
@@ -23,7 +18,7 @@ function App() {
 
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj}/> : "Initializing..."}
+      {init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj}/> : "Initializing..."}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
