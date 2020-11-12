7a2ee4b26132b16d2451714c78999ca20c2a9fd2
sinbox0701 <psh090953@gmail.com>
2020-11-12 10:13:44 +0900

    Log In

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import AppRouter from "components/Router";
 import {authService} from "fbase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => { // user가 있는지 확인하는 함수
+      if(user){
+        setIsLoggedIn(true);
+      }
+      else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  },[]);
+  //새로고침할 때 마다 작동
 
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn}/> : "Initializing..."}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,6 +5,7 @@ const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
 
     const onChange = (event) => {
         const {
@@ -31,9 +32,11 @@ const Auth = () => {
             console.log(data);
         }
         catch(error){
-            console.log(error);
+            setError(error.message);
         }
     };
+
+    const toggleAccount = () => setNewAccount((prev) => !prev); //이전의 값과 반대값 저장
 
     return (
         <div>
@@ -54,8 +57,15 @@ const Auth = () => {
                     value={password}
                     onChange={onChange}
                 />
-                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+                <input 
+                    type="submit" 
+                    value={newAccount ? "Create Account" : "Log In"} 
+                />
+                {error}
             </form>
+            <span onClick={toggleAccount}>
+                {newAccount ? "Sign In" : "Create Account"}
+            </span>
             <div>
                 <button>Continue with Google</button>
                 <button>Continue with Github</button>
